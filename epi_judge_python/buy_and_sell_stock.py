f6c746adf4cb0548e1fa94912b3bf06ab9a46b56
Ying Tsen Hong <tsenying@gmail.com>
2018-08-12 16:48:47 -0600

    first try at 5.6

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
 
 
+# O(n**2)
 def buy_and_sell_stock_once(prices):
     # TODO - you fill in here.
-    return 0.0
+    max_profit = 0.0
+    for i in range(len(prices) - 1):
+        for j in range(i, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > 0 and profit > max_profit:
+                max_profit = profit
+    return max_profit
+
+def buy_and_sell_stock_once(prices):
+    # TODO - you fill in here.
+    max_profit = 0.0
+    per_day_profit = [0.0] * len(prices)
+    for i in range(1, len(prices)):
+        
+
 
 
 if __name__ == '__main__':
