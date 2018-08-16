f1e8433f9232b0a7d9a1a2a03c0f079d78afc01c
Ying Hong <ying_hong@trimble.com>
2018-08-16 17:09:44 -0600

    6.1 working

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,37 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
-
 def int_to_string(x):
     # TODO - you fill in here.
-    return ''
+    chs=[]
+    negative = False
+    if x < 0:
+        negative = True
+        x = abs(x)
+    while True:
+        chs.append(chr(ord('0') + x % 10))
+        x //= 10
+        if x == 0:
+            break
+    if negative:
+        chs.append('-')
+    chs.reverse()
+    return ''.join(chs)
 
 
+INTS={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
 def string_to_int(s):
     # TODO - you fill in here.
-    return 0
+    n = 0
+    negative = False
+    if s[0] == '-':
+        negative = True
+        s = s[1:]
+    for i in range(len(s)):
+        n = (n * 10) + INTS[s[i]]
+    if negative:
+        n = -n
+    return n
 
 
 def wrapper(x, s):
