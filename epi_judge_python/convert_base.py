00798459e9470b18fd2168c6b7ac927f66bc7f10
Ying Hong <ying_hong@trimble.com>
2018-08-17 17:20:29 -0600

    5.12 functools.reduce version

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -46,7 +46,7 @@ def decimal_to_base(x, base):
 # Assume 2 <= b1,b2 <= 16
 # representation A=10, B=11, C=12, D=13, E=14, F=15
 # string '615' base b1 7 to base b2 13
-def convert_base(num_as_string, b1, b2):
+def x_convert_base(num_as_string, b1, b2):
     # TODO - you fill in here.
     print('num_as_string={n}, b1={b1}, b2={b2}'.format(n=num_as_string, b1=b1, b2=b2))
     decimal = string_to_decimal(num_as_string, b1)
@@ -55,6 +55,32 @@ def convert_base(num_as_string, b1, b2):
     print('number=', number)
     return number
 
+import functools, string
+def string_to_int(num_as_string, base):
+    number = functools.reduce(
+        lambda x, c: x * base + string.hexdigits.index(c.lower()),
+        num_as_string,
+        0
+    )
+    return number
+
+def construct_from_base(num_as_int, base):
+    if num_as_int == 0:
+        return ''
+    else:
+        return (construct_from_base(num_as_int // base, base) +
+            string.hexdigits[num_as_int % base].upper())
+
+def convert_base(num_as_string, b1, b2):
+    is_negative = num_as_string[0] == '-'
+    number = string_to_int(num_as_string[is_negative:], b1)
+    if number == 0:
+        target = '0'
+    else:
+        target = construct_from_base(number, b2)
+    if is_negative:
+        target = '-' + target
+    return target
 
 if __name__ == '__main__':
     exit(
