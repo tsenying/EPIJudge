e3414d6a31991ac398adc0f4289734c243da11d6
Ying Hong <tsenying@gmail.com>
2018-08-19 15:22:26 -0600

    7.3 test for list cycle

--- epi_judge_python/is_list_cyclic.py
+++ epi_judge_python/is_list_cyclic.py
@@ -4,9 +4,52 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+import list_node
+def build_linked_list(data):
+    """A helper function for linked list parser.
+
+    Constructs a linked list from a list of values.
+
+    :param data - list of values.
+    """
+    head = None
+    for x in reversed(data):
+        head = list_node.ListNode(x, head)
+    return head
+
+# make cycled list
+l = build_linked_list([1,2,3,4,5])
+start = l.next.next
+end = start.next.next
+end.next = start
 
 def has_cycle(head):
+    def cycle_len(end):
+        start, count = end, 0
+        while True:
+            count += 1
+            start = start.next
+            if start is end:
+                return count
+    #
+    #
     # TODO - you fill in here.
+    fast = slow = head
+    while fast and fast.next and fast.next.next:
+        slow, fast = slow.next, fast.next.next
+        if slow is fast:
+            print ("has cycle")
+            c_len = cycle_len(slow)
+            print ("cycle length = {}".format(c_len))
+            # start at beginning with two pointers offset by c_len
+            ptr_ahead = head
+            for _ in range(c_len):
+                ptr_ahead = ptr_ahead.next
+            ptr = head
+            while ptr is not ptr_ahead:
+                ptr, ptr_ahead = ptr.next, ptr_ahead.next
+            return ptr
+    print("NO cycle")
     return None
 
 
