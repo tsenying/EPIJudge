4fd03d59e6a6845fd2331f1b431f6198e8886247
Ying Hong <tsenying@gmail.com>
2018-08-19 21:39:56 -0600

    8.6 binary tree depth order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def binary_tree_depth_order(tree):
     # TODO - you fill in here.
-    return []
+    result = []
+    if not tree:
+        return result
+
+    curr_depth_nodes = [tree]
+    while curr_depth_nodes:
+        result.append([curr.data for curr in curr_depth_nodes])
+        curr_depth_nodes = [
+            child
+            for curr in curr_depth_nodes for child in (curr.left, curr.right)
+            if child
+        ]
+    return result
 
 
 if __name__ == '__main__':
