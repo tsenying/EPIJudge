8cdd2e9a40bcf8e4e3b164a2109156fbfd2d8dbb
Ying Hong <tsenying@gmail.com>
2018-08-22 16:30:18 -0600

    Add binary tree inorder and postorder traversals

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -1,9 +1,39 @@
 from test_framework import generic_test
+from bst_node import BstNode
 
+left_left = BstNode(4)
+left_right = BstNode(5)
+left = BstNode(1, left_left, left_right)
+right = BstNode(3)
+root = BstNode(2,left,right)
+print('root={}, left={}, right={}'.format(root,left,right))
 
-def inorder_traversal(tree):
+result = []
+def tree_traversal(tree, result, order):
     # TODO - you fill in here.
-    return []
+    if tree:
+        if order == -1:
+            print('Preorder: {}'.format(tree.data))
+            result.append(tree.data)
+        tree_traversal(tree.left, result, order)
+        if order == 0:
+            print('Inorder: {}'.format(tree.data))
+            result.append(tree.data)
+        tree_traversal(tree.right, result, order)
+        if order == 1:
+            print('Postorder: {}'.format(tree.data))
+            result.append(tree.data)
+    print('tree={}, result={}'.format(tree, result))
+    return result
+
+def preorder_traversal(tree, result):
+    result = tree_traversal(tree, result, -1)
+
+def inorder_traversal(tree, result):
+    result = tree_traversal(tree, result, 0)
+
+def postorder_traversal(tree, result):
+    result = tree_traversal(tree, result, 1)
 
 
 if __name__ == '__main__':
