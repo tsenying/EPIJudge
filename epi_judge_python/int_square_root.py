64f5abf726d07fab62a475a314faa9c60b78bd82
Ying Hong <tsenying@gmail.com>
2018-08-27 11:49:53 -0600

    11.4 compute integer square root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -3,7 +3,17 @@ from test_framework import generic_test
 
 def square_root(k):
     # TODO - you fill in here.
-    return 0
+    left, right = 0, k
+    while left <= right:
+        mid = (right - left) // 2 + left
+        mid_square = mid ** 2
+        if mid_square < k:
+            left = mid + 1
+        elif mid_square == k:
+            return mid
+        else:
+            right = mid - 1
+    return left - 1
 
 
 if __name__ == '__main__':
