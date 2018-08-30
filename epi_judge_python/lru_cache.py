d22c7d36f43b435d7d51a5f702f47cd275b71de2
Ying Hong <tsenying@gmail.com>
2018-08-30 16:37:15 -0600

    12.3 long hand solution

--- epi_judge_python/lru_cache.py
+++ epi_judge_python/lru_cache.py
@@ -1,23 +1,45 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
-
+import collections
 class LruCache:
     def __init__(self, capacity):
         # TODO - you fill in here.
+        self._isbn_price_table = collections.OrderedDict()
+        self._capacity = capacity
         return
 
     def lookup(self, isbn):
         # TODO - you fill in here.
-        return 0
+        if isbn in self._isbn_price_table:
+            # remove and re-add to put make it most recently used
+            price = self._isbn_price_table.pop(isbn)
+            self._isbn_price_table[isbn] = price
+        else:
+            price = -1
+        return price
 
     def insert(self, isbn, price):
+        print("insert params isbn={}, price={}".format(isbn, price))
         # TODO - you fill in here.
-        return
+        # if isbn is already in cache, remove and re-add so that it is last item (most recently used)
+        # in cache which is an instance of OrderedDict
+        if isbn in self._isbn_price_table:
+            price = self._isbn_price_table.pop(isbn)
+        elif len(self._isbn_price_table) == self._capacity:
+            # if at capacity, remove LRU key to make room
+            self._isbn_price_table.popitem(last=False)
+        self._isbn_price_table[isbn] = price
+        # print("insert result isbn={}, price={}".format(isbn, self._isbn_price_table[isbn]))
+        print("+insert result ", self._isbn_price_table.items())
 
     def erase(self, isbn):
         # TODO - you fill in here.
-        return True
+        if isbn in self._isbn_price_table:
+            del self._isbn_price_table[isbn]
+            return True
+        else:
+            return False
 
 
 def run_test(commands):
