62b1046381cf369932e91c9c110b49804e377e93
Ying Hong <tsenying@gmail.com>
2018-08-30 14:41:15 -0600

    12.2 long hand solution

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,7 +1,7 @@
 from test_framework import generic_test
 
 from collections import Counter
-def is_letter_constructible_from_magazine(letter_text, magazine_text):
+def pythonic_is_letter_constructible_from_magazine(letter_text, magazine_text):
     # TODO - you fill in here.
     # 1. count occurrences of each character in letter
     # 2. count occurrences of each character in magazine_text
@@ -10,6 +10,17 @@ def is_letter_constructible_from_magazine(letter_text, magazine_text):
     magazine_counts = Counter(magazine_text)
     return not (letter_counts - magazine_counts)
 
+def is_letter_constructible_from_magazine(letter_text, magazine_text):
+    letter_counts = Counter(letter_text)
+    #
+    for c in magazine_text:
+        if c in letter_counts:
+            letter_counts[c] -= 1
+            if letter_counts[c] == 0:
+                del letter_counts[c]
+                if not letter_counts:
+                    return True
+    return not letter_counts
 
 if __name__ == '__main__':
     exit(
