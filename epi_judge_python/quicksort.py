6d6c21bb52ad5a83c25f4240e3368d8917b3c23a
Ying Hong <tsenying@gmail.com>
2018-09-02 12:09:40 -0600

    13 quicksort exercise using partition algorithm from 11.8

---
+++ epi_judge_python/quicksort.py
@@ -0,0 +1,65 @@
+from test_framework import generic_test
+
+A=[2,3,1]
+def x_partition(A, lo, hi):
+    print('partition: A={},lo={},hi={}'.format(A,lo,hi))
+    i = lo # A for index < i is less than pivot value
+    j = hi # A for index > j is greater than pivot value
+    pivot_value = A[lo]
+    while True:
+        if i >= j:
+            print('partition i={}, j={}, return={}'.format(i,j,i))
+            return i
+        while A[i] < pivot_value:
+            i += 1
+        while A[j] > pivot_value:
+            j -= 1
+        A[i], A[j] = A[j], A[i]
+        i += 1
+        j -= 1
+
+import random
+def partition(A, left, right):
+    print('partition: A={},left={},right={}'.format(A,left,right))
+    # pivot_index = random.randint(left, right)
+    pivot_index = left
+    pivot_value = A[pivot_index]
+    new_pivot_index = left
+    # swap pivot value out of the way during partitioning
+    # it is swapped to new_pivot_index when partioning is done
+    A[right], A[pivot_index] = A[pivot_index], A[right]
+    for i in range(left, right):
+        if A[i] < pivot_value:
+            A[new_pivot_index], A[i] = A[i], A[new_pivot_index]
+            new_pivot_index += 1
+    # move pivot value to new_pivot_index
+    A[new_pivot_index], A[right] = A[right], A[new_pivot_index]
+    return new_pivot_index
+
+def quicksort(A, lo, hi):
+    print('quicksort: A={},lo={},hi={}'.format(A,lo,hi))
+    if lo >= hi:
+        # base case 0 or 1 element array is sorted by definition
+        print('quicksort lo >= hi')
+        return
+    else: # lo < hi:
+        print('quicksort partition')
+        pivot_index = partition(A, lo, hi)
+        print('quicksort quicksort lo={} to pivot_index={}'.format(lo, pivot_index))
+        quicksort(A, lo, pivot_index)
+        print('quicksort quicksort pivot_index+1={} to hi={}'.format(pivot_index+1, hi))
+        quicksort(A, pivot_index+1, hi)
+
+def qsort(A):
+    quicksort(A, 0, len(A) - 1)
+
+
+def stable_sort_list(L):
+    # TODO - you fill in here.
+    return None
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main("sort_list.py", 'sort_list.tsv',
+                                       stable_sort_list))
