dc05adac6f393c012c73a8cf15ee7bff6054d6af
Ying Hong <tsenying@gmail.com>
2018-09-19 17:13:14 -0600

    18.2 matrix connected regions

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,8 +1,26 @@
 from test_framework import generic_test
 
-
+import collections
 def flip_color(x, y, image):
     # TODO - you fill in here.
+    rows = len(image)
+    cols = len(image[0])
+    print('flip_color, x={}, y={}, rows={}, cols={}, image={} '.format(x, y, rows, cols, image))
+    # track initial color of coord
+    color = image[x][y]
+    q = collections.deque([(x,y)])
+    # flip color of current coord
+    image[x][y] = 1 - image[x][y]
+
+    # BFS flip color of neighbors
+    while q:
+        x, y = q.popleft()
+        for next_x, next_y in ((x-1, y), (x+1, y), (x, y-1), (x, y+1)):
+            if 0 <= next_x < rows and 0 <= next_y < cols and image[next_x][next_y] == color:
+                # flip color
+                image[next_x][next_y] = 1 - image[next_x][next_y]
+                q.append((next_x, next_y))
+
     return
 
 
