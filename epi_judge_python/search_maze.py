1b0d2e68283944de198582577212c0af76d089c5
Ying Hong <tsenying@gmail.com>
2018-09-19 16:10:05 -0600

    18.1 search maze, DFS

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -13,7 +13,54 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 def search_maze(maze, s, e):
     # TODO - you fill in here.
-    return []
+    rows = len(maze)
+    cols = len(maze[0])
+
+    def adjacents(curr):
+        # possible next steps
+        neighbors = []
+        left = Coordinate(curr.x-1, curr.y)
+        right = Coordinate(curr.x+1, curr.y)
+        top = Coordinate(curr.x, curr.y-1)
+        bottom = Coordinate(curr.x, curr.y+1)
+
+        if 0 <= left.x < rows and maze[left.x][left.y] == WHITE:
+            neighbors.append(left)
+
+        if 0 <= right.x < rows and maze[right.x][right.y] == WHITE:
+            neighbors.append(right)
+
+        if 0 <= top.y < cols and maze[top.x][top.y] == WHITE:
+            neighbors.append(top)
+
+        if 0 <= bottom.y < cols and maze[bottom.x][bottom.y] == WHITE:
+            neighbors.append(bottom)
+
+        print('---adjacents:curr={}, neighbors={}'.format(curr, neighbors))
+        return neighbors
+
+    def search_maze_dfs(curr, dest, visited=set()):
+        if curr in visited:
+            return False
+        visited.add(curr)
+
+        path.append(curr)
+        print('-search_maze_dfs: curr={}, dest={}, path={}, visited={}'.format(curr, dest, path, visited))
+
+        if curr == dest:
+            return True
+        for step in adjacents(curr):
+            print('--search_maze_dfs step=', step)
+            if search_maze_dfs(step, dest, visited):
+                return True
+        path.pop()
+        return False
+
+    print('start={}, end={}, maze dims={},{}, maze={}'.format(s, e, len(maze), len(maze[0]), maze))
+    path = []
+    search_maze_dfs(s, e)
+    print('+++search_maze path=', path)
+    return path
 
 
 def path_element_is_feasible(maze, prev, cur):
@@ -42,6 +89,10 @@ def search_maze_wrapper(executor, maze, s, e):
 
     for i in range(1, len(path)):
         if not path_element_is_feasible(maze, path[i - 1], path[i]):
+            path_i_1 = path[i - 1]
+            path_i = path[i]
+            print('path_element_is_feasible path[i - 1]={}, path[i]={}'.format(path[i - 1], path[i]))
+            print('maze(path[i - 1])={}, maze[path[i]]={}'.format(maze[path_i_1.x][path_i_1.y], maze[path_i.x][path_i.y]))
             raise TestFailure("Path contains invalid segments")
 
     return True
