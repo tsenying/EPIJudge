4b1a8ce08788590aaaa401a50f70bcf4363c6217
Ying Hong <tsenying@gmail.com>
2018-09-20 16:22:35 -0600

    15 intro: greatest common divisor using euclidean algorithm

--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
-
+# chapter 15 intro greatest common divisor
+# using Euclidean algorithm: https://en.wikipedia.org/wiki/Euclidean_algorithm
 def gcd(x, y):
     # TODO - you fill in here.
-    return 0
+    print('x={}, y={}, x%y={}'.format(x, y, (x%y if y!=0 else -1)))
+    if y == 0:
+        return x
+    else:
+        # after modulo, result will be less than y, so switch order
+        # if y > x, then x%y is x, so next recursive call flips order and
+        # it's as if initially called with gcd(y,x), just one extra call, i.e.
+        # gcd(3,14) == gcd(14,3)
+        return gcd(y, x%y)
 
 
 if __name__ == '__main__':
