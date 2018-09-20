338ea0aa9a38c95085fdad0e8b6ea77d808daa1e
Ying Hong <tsenying@gmail.com>
2018-09-20 14:18:09 -0600

    18 dynamic programming, boot camp

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+# chapter 16 dynamic programming
 
-def fibonacci(n):
+# recursive solution with exponential run time
+def fibonacci_O_exp(n, cache={}):
     # TODO - you fill in here.
-    return -1
+    if n <= 1:
+        return n
+    if n not in cache:
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return cache[n]
+
+# O(n) time in O(1) space
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    f_minus_2, f_minus_1 = 0, 1
+    for _ in range(1, n):
+        f = f_minus_2 + f_minus_1
+        f_minus_2, f_minus_1 = f_minus_1, f
+    return f
 
 
 if __name__ == '__main__':
