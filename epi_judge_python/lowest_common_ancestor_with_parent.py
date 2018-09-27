c78c9131c95865388d4c4c2a0bea234cddb4e581
Ying Hong <tsenying@gmail.com>
2018-09-27 17:02:48 -0600

    9.4 reprise

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -6,7 +6,7 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def lca(node0, node1):
+def x_lca(node0, node1):
     print('node0={}, node1={}'.format(node0, node1))
     # TODO - you fill in here.
     def get_depth(node):
@@ -30,6 +30,32 @@ def lca(node0, node1):
         node0 = node0.parent
 
     # traverse up to root until nodes match
+    while node0 is not node1:
+        node0, node1 = node0.parent, node1.parent
+    return node0
+
+def lca(node0, node1):
+    # TODO - you fill in here.
+    def node_depth(node):
+        depth = -1
+        while(node):
+            depth += 1
+            node = node.parent
+        return depth
+    # depths of each node
+    depth0, depth1 = map(node_depth, [node0, node1])
+
+    # for deeper node, traverse up until depth matches
+    if (depth0 > depth1):
+        diff = depth0 - depth1
+        for _ in range(diff):
+            node0 = node0.parent
+    if (depth1 > depth0):
+        diff = depth1 - depth0
+        for _ in range(diff):
+            node1 = node1.parent
+
+    # travese up concurrently until LCA found
     while node0 is not node1:
         node0, node1 = node0.parent, node1.parent
     return node0
