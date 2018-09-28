d4537b7c00d56bd6861f328cc6bcfcddabc7b087
Ying Hong <tsenying@gmail.com>
2018-09-28 15:02:56 -0600

    9.2 reprise

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -1,11 +1,11 @@
 from test_framework import generic_test
 
 
-def is_symmetric(tree):
+def x_is_symmetric(tree):
     # TODO - you fill in here.
     if tree is None:
         return True
-        
+
     def check_symmetry(left, right):
         # base case: left is None and right is None
         if left is None and right is None:
@@ -20,6 +20,27 @@ def is_symmetric(tree):
 
     return check_symmetry(tree.left, tree.right)
 
+def is_symmetric(tree):
+    # TODO - you fill in here.
+    print('tree=', tree)
+    if tree is None:
+        return True
+
+    def check_symmetry(left, right):
+        if left is None and right is None:
+            # base case
+            return True
+        elif left and right:
+            # both sides present
+            if left.data == right.data and \
+                check_symmetry(left.left, right.right) and \
+                check_symmetry(left.right, right.left):
+                return True
+        # one side empty
+        return False
+
+
+    return check_symmetry(tree.left, tree.right)
 
 if __name__ == '__main__':
     exit(
