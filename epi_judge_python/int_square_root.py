8d5111b0f943baafc1a743187e22313bde1f40f3
Ying Hong <tsenying@gmail.com>
2018-09-30 13:38:15 -0600

    11.4 reprise

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -1,7 +1,7 @@
 from test_framework import generic_test
 
 
-def square_root(k):
+def x_square_root(k):
     # TODO - you fill in here.
     left, right = 0, k
     while left <= right:
@@ -14,6 +14,30 @@ def square_root(k):
         else:
             right = mid - 1
     return left - 1
+
+# Given a non-negative integer
+# find the largest number whose square is less than the input number
+def square_root(k):
+    # find by bisection
+    # halve the number and get square
+    # if square == number, return largest
+    # if midpoint**2 < number
+    #   record as largest
+    #   midpoint = L + (U - M) // 2
+    # elif midpoint**2 > number
+    #   midpoint = L + (M - L) // 2
+    L, U = 0, k
+    while L <= U:
+        mid = L + (U - L) // 2
+        mid_square = mid ** 2
+        if mid_square <= k:
+            # look to higher side
+            L = mid + 1
+        else:
+            # mid_square > k, look to lower side
+            U = mid - 1
+    return U
+
 
 
 if __name__ == '__main__':
