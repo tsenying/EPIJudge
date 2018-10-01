79e4a140084cda24c7c7a5ec937fb929705ae3f1
Ying Hong <tsenying@gmail.com>
2018-10-01 16:56:14 -0600

    13.1 reprise

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,7 +1,7 @@
 from test_framework import generic_test
 
 
-def intersect_two_sorted_arrays(A, B):
+def x_intersect_two_sorted_arrays(A, B):
     # TODO - you fill in here.
     # iterate through both arrays concurrently and look for matches
     intersect = [] # common elements in both arrays, no duplicates
@@ -20,6 +20,32 @@ def intersect_two_sorted_arrays(A, B):
             b += 1
     return intersect
 
+def intersect_two_sorted_arrays(A, B):
+    # advance through arrays concurrently
+    intersect = list()
+    # maintain pointers into A, B
+    a, b = 0, 0
+    # print('a={}, A[a]={}, b={}, B[b]'.format(a,A[a],b,B[b]))
+    while a < len(A) and b < len(B):
+        print('a={}, A[a]={}, b={}, B[b]={}'.format(a,A[a],b,B[b]))
+        if A[a] == B[b]:
+            intersect.append(A[a])
+            a, b = a + 1, b + 1
+            while a < len(A) and A[a] == A[a - 1]:
+                a += 1
+            while b < len(B) and B[b] == B[b - 1]:
+                b += 1
+            print('A[a] == B[b], a={}, b={}, intersect={}'.format(a, b, intersect))
+        elif A[a] < B[b]:
+            while a < len(A) and A[a] < B[b]:
+                a += 1
+                print('A[a] < B[b] a=', a)
+        else:
+            while b < len(B) and A[a] > B[b]:
+                b += 1
+                print('A[a] > B[b] b=', b)
+    print('!!! intersect=', intersect)
+    return intersect
 
 if __name__ == '__main__':
     exit(
